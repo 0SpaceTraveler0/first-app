fbe5c8c052dc3520452158bb7ebfded314b4be8d
0SpaceTraveler0 <t34ilya2014@mail.ru>
2023-07-18 11:36:19 +0500

    first com

--- php/src/index.php
+++ php/src/index.php
@@ -1,10 +1,14 @@
 <?php
 
 echo phpinfo();
+$x = 1;
+$y = 2;
+$qwe = $x + $y;
+
 
 //These are the defined authentication environment in the db service
 
-/* // The MySQL service named in the docker-compose.yml.
+ // The MySQL service named in the docker-compose.yml.
 $host = 'db';
 
 // Database use name
@@ -32,5 +36,5 @@ foreach ($users as $user) {
     echo "<br>";
     echo $user->username . " " . $user->password;
     echo "<br>";
-} */
+}
 ?>
